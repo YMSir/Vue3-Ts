c767d54a092c95fc71dd818588ddf4e971520ce9
ynf <wyoihahds@163.com>
2021-08-25 16:46:38 +0800

    vue3-ts

---
+++ src/views/user/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login-bg"></div>
+</template>
+
+<script lang="ts" setup>
+function getUser (ss: string) {
+  return { ss };
+}
+
+const { ss } = getUser('ss');
+console.log(ss);
+</script>
+
+<style scoped>
+</style>
