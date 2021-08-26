1a9c91d983efc8fd0ce6bb3efe3853a0a47bdc31
ynf <wyoihahds@163.com>
2021-08-26 09:42:56 +0800

    vue3-ts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
-  <!--<img alt="Vue logo" src="./assets/logo.png" />-->
-  <!--<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />-->
-  <Login></Login>
+  <login></login>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
 import Login from './views/user/Login.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    Login,
-    HelloWorld
-  }
-})
 </script>
