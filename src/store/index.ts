fdc95918b2a1d9f2bb68956a871d10b6965dfa42
mac <yes_man_yu@163.com>
2021-08-31 07:36:17 +0800

    vue3 ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,14 +7,14 @@ import getters from './getters';
 import type { ModuleTree } from 'vuex';
 
 const files = import.meta.glob('./modules/*.ts');
-const modules:ModuleTree<string> = {}
+const modules: ModuleTree<string> = {};
 
 for (const path in files) {
   files[path]().then(mod => {
     if (/^\.\/modules\/(.*)\.ts$/.test(path)) {
-      modules[RegExp.$1] = mod.default
+      modules[RegExp.$1] = mod.default;
     }
-  })
+  });
 }
 
 export default createStore({
